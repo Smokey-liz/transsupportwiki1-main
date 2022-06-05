20932ce17e677f415c40a148cde341baa465217b
Smokey-liz <elizahofer02@outlook.com>
2022-06-04 23:23:12 -0400

    trying this

--- tswsuggestions.js
+++ tswsuggestions.js
@@ -1,11 +1,11 @@
 function sendEmail() {
 	Email.send({
 	Host: "smtp.gmail.com",
-	Username : "<sender’s email address>",
-	Password : "<email password>",
-	To : '<recipient’s email address>',
-	From : "<sender’s email address>",
-	Subject : "<email subject>",
+	Username : "tswelizahofer@gmail.com",
+	Password : "Ee391001771Ee",
+	To : 'elizahofer02@outlook.com',
+	From : "tswelizahofer@gmail.com",
+	Subject : "tsw suggestion",
 	Body : "<email body>",
 	}).then(
 		message => alert("mail sent successfully")
